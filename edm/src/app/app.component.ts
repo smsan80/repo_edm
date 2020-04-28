1b272faa728742c18a8636a0634d60d112034a9c
Pragnesh Makwana <info@webdroids.in>
2020-04-28 20:54:37 +0530

    update

--- edm/src/app/app.component.ts
+++ edm/src/app/app.component.ts
@@ -15,33 +15,33 @@ export class AppComponent {
     private _register: RegisterService) { }
 
   ngOnInit(): void {
-    this._base._encryptedStorage.get(enAppSession.IsGuestUser).then(IsGuestUser => {
-      if (!IsGuestUser) {
-        this.registerGuest();
-      }
+    // this._base._encryptedStorage.get(enAppSession.IsGuestUser).then(IsGuestUser => {
+    //   if (!IsGuestUser) {
+    //     this.registerGuest();
+    //   }
 
-    });
+    // });
   }
-  registerGuest() {
-    Helpers.setLoading(true);
-    this._register.registerGuest().subscribe((res: any) => {
-      if (res != undefined && res != null && res != '') {
-        //setRef_ Gest_GUID in Ref_User_GUID
-        let ref_GuestUser_ID = res.split('~')[0];
-        let ref_User_ID = res.split('~')[1];
-        this._base._encryptedStorage.set(enAppSession.Ref_GuestUser_ID, ref_GuestUser_ID);
-        this._base._encryptedStorage.set(enAppSession.Ref_User_ID, ref_User_ID);
-        this._base._encryptedStorage.set(enAppSession.IsGuestUser, true);
-      }
-    }, error => {
-      Helpers.setLoading(false);
-    });
-    setTimeout(() => {
-      Helpers.setLoading(true);
-      this._base._router.navigate(['home']);
-    }, 1000);
-    // } else {
-    //   this._base._CommonService.showAlert("You are appear to be offline. Please try again later!.", true);
-    // }
-  }
+  // registerGuest() {
+  //   Helpers.setLoading(true);
+  //   this._register.registerGuest().subscribe((res: any) => {
+  //     if (res != undefined && res != null && res != '') {
+  //       //setRef_ Gest_GUID in Ref_User_GUID
+  //       let ref_GuestUser_ID = res.split('~')[0];
+  //       let ref_User_ID = res.split('~')[1];
+  //       this._base._encryptedStorage.set(enAppSession.Ref_GuestUser_ID, ref_GuestUser_ID);
+  //       this._base._encryptedStorage.set(enAppSession.Ref_User_ID, ref_User_ID);
+  //       this._base._encryptedStorage.set(enAppSession.IsGuestUser, true);
+  //     }
+  //   }, error => {
+  //     Helpers.setLoading(false);
+  //   });
+  //   setTimeout(() => {
+  //     Helpers.setLoading(true);
+  //     this._base._router.navigate(['home']);
+  //   }, 1000);
+  //   // } else {
+  //   //   this._base._CommonService.showAlert("You are appear to be offline. Please try again later!.", true);
+  //   // }
+  // }
 }
