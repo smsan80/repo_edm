0dbcee7ca0e39ddf2d143e122814c73830b53979
Pallavi Chapla <pallavi.chapla@gmail.com>
2020-05-01 18:10:25 +0530

    producer profile page design added

--- edm/src/app/profiles/producer/producer.component.ts
+++ edm/src/app/profiles/producer/producer.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'app-producer',
     templateUrl: './producer.component.html',
-    styleUrls: ['./producer.component.scss']
+    styleUrls: ['./producer.component.scss','../../widgets/featuredProductSlider/featuredProductSlider.component.scss']
 })
 
 export class ProducerProfileComponent implements OnInit {
