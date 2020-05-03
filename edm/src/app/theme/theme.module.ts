a68c840c500c1306d3196657e9130262ad75367b
Pragnesh Makwana <info@webdroids.in>
2020-05-03 12:27:55 +0530

    update

--- edm/src/app/theme/theme.module.ts
+++ edm/src/app/theme/theme.module.ts
@@ -1,10 +1,6 @@
 // import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { BodyComponent } from './client/body/body.component';
-import { HeaderComponent } from './client/header/header.component';
-import { FooterComponent } from './client/footer/footer.component';
-import { ThemeComponent } from './theme.component';
 import { ThemeRoutingModule } from './theme-routing.module';
 import { HomeModule } from '../home/home.module';
 import { LoginModule } from '../auth/login/login.module';
