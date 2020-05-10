e2c5dfdd6cc7c94084a1ab9ab84b61b83a8860bc
Pragnesh Makwana <info@webdroids.in>
2020-05-10 18:30:40 +0530

    update

---
+++ edm/src/assets/js/edm.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $(".menu_navigation").click(function () {
+        debugger
+        $(".admin_base").toggleClass("admin_mini_menu");
+    });
+
+    $(".mobile_search").click(function () {
+        $(".mobile_search_section").show();
+    });
+
+    $(".search_section > img").click(function () {
+        $(".mobile_search_section").hide();
+    });
+
+    if (window.matchMedia("(max-width: 850px)").matches) {
+        $(".admin_base").addClass("admin_mini_menu");
+    } else {
+        $(".admin_base").removeClass("admin_mini_menu");
+    }
+});
+$(document).ready(function () {
+    $(".open_mobile_menu").click(function () {
+        $(".mobile_menu").show();
+    });
+});
